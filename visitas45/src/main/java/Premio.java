3f32b2bd9ed8255b75572223344e9f355ae929c8
Marcos Levis <marcoslevis.rck@gmail.com>
2024-09-07 20:38:28 -0300

    visitas 45

---
+++ visitas45/src/main/java/Premio.java
@@ -0,0 +1,48 @@
+
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class Premio
+ */
+@WebServlet("/Premio")
+public class Premio extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	private String mensaje = "Hola, como estas nombre";
+    /**
+     * Default constructor. 
+     */
+    public Premio() {
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.setContentType("text/html");
+		PrintWriter out = response.getWriter();
+		out.println("<html><body>");
+		String nombreNuevo =request.getParameter("nombre");
+		out.println("<h1>"+ this.mensaje.replace("nombre", nombreNuevo)+ " </h1>");
+		out.println("</body></html>");
+		out.close();
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
